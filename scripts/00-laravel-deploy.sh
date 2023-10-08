f62b4bfe7305580f736b051ee9c13dba48e2326e
ash08642 <108698229+ash08642@users.noreply.github.com>
2023-10-08 17:46:24 +0200

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
@@ -8,6 +7,10 @@ php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
+
+echo "Running vite..."
+npm install
+npm run build
 
 echo "Running migrations..."
 php artisan migrate --force
